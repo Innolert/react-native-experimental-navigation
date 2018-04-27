e5f02dec111868b56514d88eb98f789b573b804b
Maxim Toyberman <maxtoyberman@gmail.com>
2018-04-27 10:25:27 +0300

    Update NavigationHeaderBackButton.js

--- NavigationHeaderBackButton.js
+++ NavigationHeaderBackButton.js
@@ -20,6 +20,7 @@ const React = require('react');
 const ReactNative = require('react-native');
 const NavigationContainer = require('./NavigationContainer');
 const NavigationRootContainer = require('./NavigationRootContainer');
+var PropTypes = require('prop-types');
 
 const {
   Image,
@@ -39,7 +40,7 @@ const NavigationHeaderBackButton = (props: Props) => (
 );
 
 NavigationHeaderBackButton.propTypes = {
-  onNavigate: React.PropTypes.func.isRequired
+  onNavigate: PropTypes.func.isRequired
 };
 
 const styles = StyleSheet.create({
